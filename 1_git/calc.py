5898ec34d1e771dce0f0b395b7834b65aa6b5735
nihal <nihal1011ML@gmail.com>
2019-01-17 10:14:52 +0530

    substract included

--- 1_git/calc.py
+++ 1_git/calc.py
@@ -3,14 +3,19 @@ import sys
 
 while(True):
 	print("1 : Add two numbers")
-	print("2 : To exit")
+	print("2 : Add subtract numbers")
+	print("3 : To exit")
 	print("Enter your choice")
 	choice = raw_input()
 	if choice == '1':
 		a = float(raw_input())
 		b = float(raw_input())
 		print("result = " +  str(a + b))
-	elif choice == '2':
+	elif choice == '1':
+		a = float(raw_input())
+		b = float(raw_input())
+		print("result = " +  str(a - b))
+	elif choice == '3':
 		sys.exit()
 	else:
 		print('invalid choice')
